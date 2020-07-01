c126d621c1d417189a2faaef110acd0af8ac0353
Jacob Vaandrager <jacobvaandrager@icloud.com>
2020-07-01 12:47:30 +0200

    added map

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-alert('Hello!');
+// Set api token
+mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vZGNyaXRpY25sIiwiYSI6ImNrYzM4NmxzcDA3YmgydHF1dmVjOWd2MXMifQ.ZzIgDo6sPpNVozhPQ_nMdg';
+
+// Initialate map
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [4.322840, 52.067101],
+  zoom: 11.15
+});
+
+// Voeg location search
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+  }),
+  'bottom-left'
+);
