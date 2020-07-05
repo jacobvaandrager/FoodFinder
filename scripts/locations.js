246151efec03ad731bd3f0e3a0f2d4b9c0398dac
Jacob Vaandrager <jacobvaandrager@icloud.com>
2020-07-05 22:48:00 +0200

    fdfg

---
+++ scripts/locations.js
@@ -0,0 +1,34 @@
+myLocationsList = [{
+    'type': 'Feature',
+    'properties': {
+      'description': '<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
+      'icon': 'theatre'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [4.313340, 52.079840]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a Mad Men Season Five Finale Watch Party, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>',
+      'icon': 'theatre'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-77.003168, 38.894651]
+    }
+  },
+  {
+    'type': 'Feature',
+    'properties': {
+      'description': '<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>',
+      'icon': 'bar'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [-77.090372, 38.881189]
+    }
+  }
+  }];
--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vZGNyaXRpY25sIiwiYSI6ImNrYzM4NmxzcDA3Ymgyd
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [4.322840, 52.067101],
-  zoom: 11.15
+  center: [4.313340, 52.079840],
+  zoom: 15
 });
 
 // Voeg location search
@@ -17,3 +17,66 @@ map.addControl(
   }),
   'bottom-left'
 );
+
+map.on('style.load', function (e) {
+    map.addSource('markers', {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [4.313340, 52.079840]
+                },
+                "properties": {
+                    "title": "Mapbox DC",
+                    "marker-symbol": "default_marker"
+                }
+            }, 
+
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-122.414, 37.776]
+                },
+                "properties": {
+                    "title": "Mapbox SF",
+                    "marker-color": "#ff00ff",
+                    "marker-symbol": "secondary_marker"
+                }
+            }
+
+            
+
+
+            ]
+        }
+    });
+
+    map.addLayer({
+        "id": "markers",
+        "source": "markers",
+        "type": "circle",
+        "paint": {
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+        }
+    });
+    let popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+    map.on("mouseenter", "markers", e => {
+      map.getCanvas().style.cursor = "pointer";
+      popup
+          .setLngLat(map.unproject(e.point))
+          .setHTML("<h3>" + e.features[0].properties.title + "</h3>")
+          .addTo(map);
+    });
+    map.on("mouseleave", "markers", () => {
+        map.getCanvas().style.cursor = "";
+        popup.remove();
+    });
+});
